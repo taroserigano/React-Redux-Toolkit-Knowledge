40b409eecc3e5be508883a7eb98f5bcee127979c
Taro Serigano <taroserigano@gmail.com>
2023-01-29 13:21:21 -0600

    Create customHooks.js

---
+++ customHooks.js
@@ -0,0 +1,10 @@
+
+
+[value, setValue] = useCustom("") 
+
+const useCustom = () => { 
+  
+  return [value, setValue] <----- these two values will be the returned values exactly same as [value, setValue]  above
+} 
+
+
